b83b3ac6d727b5878c96687611ec7da84c6bdf5c
trippli <trippli@tencent.com>
2019-05-18 18:26:41 +0800

    init

---
+++ centos/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -ex
+
+user_id=${USER_ID:-9001}
+user_name=${USER_NAME:-"user"}
+
+if [ `cat /etc/passwd | cut -f1 -d':' | grep -w "$user_name" -c` -eq 0 ]; then
+    useradd -s /bin/zsh -u $user_id -o -m $user_name
+    sed -i 's/^##includedir \/etc\/sudoers.d/#includedir \/etc\/sudoers.d/g' /etc/sudoers; \
+    echo "$user_name ALL=(ALL)       NOPASSWD: ALL" > /etc/sudoers.d/$user_name
+
+    cp -r -p ~/.oh-my-zsh ~/.zshrc ~/.zprofile /home/$user_name/
+    chown $user_name:$user_name -R /home/$user_name/
+    chown $user_name:$user_name /data
+fi
+
+exec /usr/local/bin/gosu "$user_name" "$@"
