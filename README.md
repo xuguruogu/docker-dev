6d9f8fb0600fdd1f18cc82dd378149c9028ede40
xuguruogu <06libenli@163.com>
2019-10-29 19:58:04 +0800

    u

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 
 ## Use
 
-    docker run -v ${HOME}:/data -e USER_NAME=$(id -un) -e USER_ID=$(id -u) --rm -it xuguruogu/dev:base
+    docker run -v $(pwd):/data -e USER_NAME=$(id -un) -e USER_ID=$(id -u) -it --name base_dev xuguruogu/dev:base
+    docker start -i base_dev
+    docker exec -it -u $(id -u) base_dev /bin/bash
 
 ## Build
     
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,15 @@ set -ex
 user_id=${USER_ID:-0}
 user_name=${USER_NAME:-"root"}
 
-if [[ `cat /etc/passwd | cut -f1 -d':' | grep -w "$user_name" -c` -eq 0 && $user_id -ne 0 ]]; then
+if [[ "$user_name" == "root" || $user_id -eq 0 ]]; then
+    exec "$@"
+    exit 0
+fi
+
+if [[ `cat /etc/passwd | cut -f1 -d':' | grep -w "$user_name" -c` -eq 0 ]]; then
     useradd -s /bin/zsh -u $user_id -o -m $user_name
     sed -i 's/^##includedir \/etc\/sudoers.d/#includedir \/etc\/sudoers.d/g' /etc/sudoers
     echo "$user_name ALL=(ALL)       NOPASSWD: ALL" > /etc/sudoers.d/$user_name
-
-    exec /usr/local/bin/gosu "$user_name" "$@"
-else
-    exec "$@"
 fi
+
+exec /usr/local/bin/gosu "$user_name" "$@"
