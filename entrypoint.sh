9b2c812e466554f81946b5b4798af63e1beac7e5
trippli <trippli@tencent.com>
2021-01-19 12:42:54 +0800

    u

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -ex
+set -e
 
 user_id=${USER_ID:-0}
 user_name=${USER_NAME:-"root"}
@@ -10,7 +10,7 @@ if [[ "$user_name" == "root" || $user_id -eq 0 ]]; then
 fi
 
 if [[ `cat /etc/passwd | cut -f1 -d':' | grep -w "$user_name" -c` -eq 0 ]]; then
-    useradd -u $user_id -o -m $user_name && yes password | passwd $user_name
+    useradd -u $user_id -o -m $user_name && yes helloworld007 | passwd $user_name
     sed -i 's/^##includedir \/etc\/sudoers.d/#includedir \/etc\/sudoers.d/g' /etc/sudoers
     echo "$user_name ALL=(ALL)       NOPASSWD: ALL" > /etc/sudoers.d/$user_name
     /usr/sbin/sshd
