f3ce91d2bf949fee3369d9fcf4ecf2b94ef42c4b
李本利 <trippli@kuaishou.com>
2022-12-06 11:08:41 +0800

    u

--- wegraphdb/build-centos8-dev-clang.sh
+++ wegraphdb/build-centos8-dev-clang.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -ex
 cd "$(dirname "${BASH_SOURCE[0]}")"
-TAG="registry-internal.corp.kuaishou.com/kgraph/ci:centos8-dev-clang15"
+TAG="wegraphdb/ci:centos8-dev-clang15"
 docker build --force-rm --tag "$TAG" --network host -f centos8-dev-clang.Dockerfile .
 docker push "$TAG"
